d2192c2d75d0772de7054b73d96c86055a8c60f6
AlexeyVarenko <alexey_varenko@mail.ru>
2022-01-04 18:22:52 +0300

    Update Solutions_7kyu.js

--- Solutions_7kyu.js
+++ Solutions_7kyu.js
@@ -118,3 +118,23 @@ function nthSmallest(arr, pos){
     return result[pos-1];
 }
 
+//Small enough? - Beginner
+
+/*You will be given an array and a limit value. You must check that all values in the array are below or equal to the limit value. If they are, return true. Else, return false.
+
+You can assume all values in the array are numbers.*/
+
+function smallEnough(a, limit){
+  let result=a[0];
+    for(let i=1; i<a.length; i++){
+      if (a[i]>result){
+        result=a[i]
+      } 
+    }
+    if(result<=limit){
+      return true;
+    }else{
+      return false;
+    }
+  }
+
