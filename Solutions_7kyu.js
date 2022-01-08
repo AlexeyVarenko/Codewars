7682cb5951aacd6afaf8eb6629eccd7ff009720b
AlexeyVarenko <alexey_varenko@mail.ru>
2022-01-08 13:46:48 +0300

    Update Solutions_7kyu.js

--- Solutions_7kyu.js
+++ Solutions_7kyu.js
@@ -170,3 +170,17 @@ function sabb(s, val, happiness){
  
 
 
+/*Double Every Other
+
+Write a function that doubles every second integer in a list, starting from the left.
+
+Example:
+
+doubleEveryOther([1,2,3,4]) => [ 1, 4, 3, 8 ]*/
+
+function doubleEveryOther(a) {
+  for (i=1; i<a.length;i+=2){
+    a[i]=a[i]*2
+  }
+  return a;
+}
