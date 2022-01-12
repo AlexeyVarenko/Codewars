8c0b864ce2fe7230cab67c730981bf75ccc4a55c
AlexeyVarenko <alexey_varenko@mail.ru>
2022-01-12 08:03:50 +0300

    Update Solutions_7kyu.js

--- Solutions_7kyu.js
+++ Solutions_7kyu.js
@@ -314,4 +314,16 @@ function paul(x) {
         }
 24
   }
- Excellent! You 
+
+
+  /*JavaScript Array Filter
+  JavaScript Arrays support a filter function (starting in JavaScript 1.6). Use the filter functionality to complete the function given.
+
+The solution would work like the following:
+
+getEvenNumbers([2,4,5,6]) // should == [2,4,6]*/
+
+function getEvenNumbers(numbersArray){
+  let result=numbersArray.filter(n=>n%2===0);
+  return result;
+}
