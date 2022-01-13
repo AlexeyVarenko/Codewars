14c85b25a44ac90ddc7dc2c42b79176178a2b4c4
AlexeyVarenko <alexey_varenko@mail.ru>
2022-01-13 08:21:20 +0300

    Update Solutions_7kyu.js

--- Solutions_7kyu.js
+++ Solutions_7kyu.js
@@ -327,3 +327,20 @@ function getEvenNumbers(numbersArray){
   let result=numbersArray.filter(n=>n%2===0);
   return result;
 }
+
+/*Stanton measure
+
+The Stanton measure of an array is computed as follows: count the number of 1s in the array. Let this count be n. The Stanton measure is the number of times that n appears in the array.
+
+Write a function which takes an integer array and returns its Stanton measure.
+
+Examples
+The Stanton measure of [1, 4, 3, 2, 1, 2, 3, 2] is 3, because 1 occurs 2 times in the array and 2 occurs 3 times.
+
+The Stanton measure of [1, 4, 1, 2, 11, 2, 3, 1] is 1, because 1 occurs 3 times in the array and 3 occurs 1 time.*/
+
+function stantonMeasure(array){
+  let amount=array.filter(x=>x===1).length;
+  let result=array.filter(x=>x===amount).length;
+  return result;
+}
