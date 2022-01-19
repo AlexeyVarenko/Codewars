91d11b8eec1f05e3b343e1e48b1957375100cc8e
AlexeyVarenko <alexey_varenko@mail.ru>
2022-01-19 10:00:00 +0300

    Update Solutions_7kyu.js

--- Solutions_7kyu.js
+++ Solutions_7kyu.js
@@ -406,3 +406,32 @@ function minMax(arr){
   return [min, max];
    
   }
+
+  /*
+  Scrolling Text
+  Let's create some scrolling text!
+
+Your task is to complete the function which takes a string, and returns an array with all possible rotations of the given string, in uppercase.
+
+Example
+scrollingText("codewars") should return:
+
+[ "CODEWARS",
+  "ODEWARSC",
+  "DEWARSCO",
+  "EWARSCOD",
+  "WARSCODE",
+  "ARSCODEW"
+  "RSCODEWA",
+  "SCODEWAR" ]*/
+  
+
+  function scrollingText(text){
+    let result = []
+    
+    for( var i = 0; i < text.length; i++){
+      result.push((text.slice(i) + text.slice(0,i)).toUpperCase())
+    }
+    
+    return result
+  }
