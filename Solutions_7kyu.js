9ea4bc5d5aa5559d71a89cf861e222336e2c8f3e
AlexeyVarenko <alexey_varenko@mail.ru>
2022-02-17 12:28:45 +0300

    Update Solutions_7kyu.js

--- Solutions_7kyu.js
+++ Solutions_7kyu.js
@@ -659,3 +659,20 @@ function getMiddle(s) {
     return (s[Math.floor(s.length / 2) - 1] + s[Math.floor(s.length / 2)])
   }
 }
+
+
+
+/*Hello World - Without Strings
+You need to create a function, helloWorld, that will return the String Hello, World! without actually using raw strings. This includes quotes, double quotes and template strings. You can, however, use the String constructor and any related functions.
+
+You cannot use the following:
+
+"Hello, World!"
+'Hello, World!'
+`Hello, World!`
+Good luck and try to be as creative as possible! */
+
+const helloWorld = () => {
+  let result=String.fromCharCode(72, 101, 108, 108, 111, 44, 32, 87, 111, 114, 108, 100, 33);
+  return result;
+};
