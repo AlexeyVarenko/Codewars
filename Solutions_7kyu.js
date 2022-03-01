d5dca690febbbb648b3498903a06827c789300aa
AlexeyVarenko <alexey_varenko@mail.ru>
2022-03-01 12:41:53 +0300

    Update Solutions_7kyu.js

--- Solutions_7kyu.js
+++ Solutions_7kyu.js
@@ -908,3 +908,18 @@ Note - there will be no punctuation or digits in the input string, only letters.
 function validSpacing(s) {
   return s.trim() == s && !s.includes("  ");
 }
+
+
+/*Frequency sequence
+Return an output string that translates an input string s/$s by replacing each character in s/$s with a number representing the number of times that character occurs in s/$s and separating each number with the character(s) sep/$sep.
+
+freq_seq("hello world", "-"); // => "1-1-3-3-2-1-1-2-1-3-1"
+freq_seq("19999999", ":"); // => "1:7:7:7:7:7:7:7"
+freq_seq("^^^**$", "x"); // => "3x3x3x2x2x1" */
+
+function freqSeq(str, sep) {
+  let arr=str.split('');
+ let arr2= arr.map((v, i, arr) => arr.filter(vi => vi === v).length);
+let result=arr2.join(sep);
+return (result);
+}
