214c4d00573dcf9f9e1e66936115d11763b39772
AlexeyVarenko <alexey_varenko@mail.ru>
2022-03-15 12:48:33 +0300

    Update Solutions_7kyu.js

--- Solutions_7kyu.js
+++ Solutions_7kyu.js
@@ -1136,3 +1136,24 @@ function tapCodeTranslation(text) {
   }
   return (Code.trim());
  }
+
+
+
+ /*Least Larger
+ Task
+Given an array of numbers and an index, return the index of the least number larger than the element at the given index, or -1 if there is no such index ( or, where applicable, Nothing or a similarly empty value ).
+
+Notes
+Multiple correct answers may be possible. In this case, return any one of them.
+The given index will be inside the given array.
+The given array will, therefore, never be empty.
+
+Example
+leastLarger( [4, 1, 3, 5, 6], 0 )  =>  3
+leastLarger( [4, 1, 3, 5, 6], 4 )  => -1 */
+
+function leastLarger(a,i) {
+  let max = a.filter((v) => v > a[i]);
+  let result= max.sort((a, b) => a - b);
+  return a.lastIndexOf(result[0]);
+}
