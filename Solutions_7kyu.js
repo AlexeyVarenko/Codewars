45d6ea7a48a06a4e5ee7448bb23af8f8bf0aba44
AlexeyVarenko <alexey_varenko@mail.ru>
2022-03-22 13:01:04 +0300

    Update Solutions_7kyu.js

--- Solutions_7kyu.js
+++ Solutions_7kyu.js
@@ -1373,3 +1373,69 @@ function hydrate(s) {
       return (`${x} glasses of water`)
     }
   }
+
+  /*Easy wallpaper
+  John wants to decorate the walls of a room with wallpaper. He wants a fool-proof method for getting it right.
+
+John knows that the rectangular room has a length of l meters, a width of w meters, a height of h meters. The standard width of the rolls he wants to buy is 52 centimeters. The length of a roll is 10 meters. He bears in mind however, that it’s best to have an extra length of wallpaper handy in case of mistakes or miscalculations so he wants to buy a length 15% greater than the one he needs.
+
+Last time he did these calculations he got a headache, so could you help John?
+
+Task
+Your function wallpaper(l, w, h) should return as a plain English word in lower case the number of rolls he must buy.
+
+Example:
+wallpaper(4.0, 3.5, 3.0) should return "ten"
+
+wallpaper(0.0, 3.5, 3.0) should return "zero"
+
+Notes:
+all rolls (even with incomplete width) are put edge to edge
+
+0 <= l, w, h (floating numbers); it can happens that w * h * l is zero
+
+the integer r (number of rolls) will always be less or equal to 20
+
+FORTH: the number of rolls will be a positive or null integer (not a plain English word; this number can be greater than 20)
+
+In Javascript English numbers are preloaded and can be accessed as:
+
+numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve","thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen", "twenty"]
+For other languages it is not preloaded but you can use the above one if you need it. */
+
+function wallpaper(l, w, h) {
+  let numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve","thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen", "twenty"]
+  if(l*w*h===0){
+    return (numbers[0])
+  } else{
+    let result=Math.ceil((2*(l+w)*h)*1.15/5.2);
+   return(numbers[result]);
+  }
+  }
+
+
+  /*Largest 5 digit number in a series
+  
+  In the following 6 digit number:
+
+283910
+91 is the greatest sequence of 2 consecutive digits.
+
+In the following 10 digit number:
+
+1234567890
+67890 is the greatest sequence of 5 consecutive digits.
+
+Complete the solution so that it returns the greatest sequence of five consecutive digits found within the number given. The number will be passed in as a string of only digits. It should return a five digit integer. The number passed may be as large as 1000 digits.*/
+
+function solution(digits){
+  let result = 0;
+
+ for (let i=0; i<digits.length; i++){
+   let number = digits.substr(i, 5);  
+   if(Number(number) > result){       
+    result = Number(number);
+   }
+ }
+ return result; 
+}
