9fde6bd8b4249c0d01b2051ec362f8a6ab4c1751
AlexeyVarenko <alexey_varenko@mail.ru>
2022-03-28 11:45:41 +0300

    Update Solutions_7kyu.js

--- Solutions_7kyu.js
+++ Solutions_7kyu.js
@@ -1542,3 +1542,29 @@ function palindrome(num) {
   if(typeof num !== 'number' || num < 0) return 'Not valid'
  return num == num.toString().split('').reverse().join('') 
 }
+
+/*Thinkful - List and Loop Drills: Lists of lists
+
+You have a two-dimensional list in the following format:
+
+data = [[2, 5], [3, 4], [8, 7]]
+Each sub-list contains two items, and each item in the sub-lists is an integer.
+
+Write a function process_data()/processData() that processes each sub-list like so:
+
+[2, 5] --> 2 - 5 --> -3
+[3, 4] --> 3 - 4 --> -1
+[8, 7] --> 8 - 7 --> 1
+and then returns the product of all the processed sub-lists: -3 * -1 * 1 --> 3.
+
+For input, you can trust that neither the main list nor the sublists will be empty.*/
+
+function processData(data){
+  let result =1
+for(let i=0; i<data.length; i++){
+     result *=data[i][0]-data[i][1]
+
+}
+return (result)
+  
+}
