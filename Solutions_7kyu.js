3d2a184e3310894779feacaa20c4a8c562abf3bc
AlexeyVarenko <alexey_varenko@mail.ru>
2022-04-04 14:07:25 +0300

    Update Solutions_7kyu.js

--- Solutions_7kyu.js
+++ Solutions_7kyu.js
@@ -1694,3 +1694,23 @@ function sort(initialArray, sortingArray) {
   }
   return result
 }
+
+
+/*Return the closest number multiple of 10
+Given a number return the closest number to it that is divisible by 10.
+
+Example input:
+
+22
+25
+37
+Expected output:
+
+20
+30
+40 */
+
+const closestMultiple10 = num => {
+  let result=Math.round(num/10)
+  return (result*10)
+  };
