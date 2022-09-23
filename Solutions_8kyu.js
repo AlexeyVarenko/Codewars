9e8c1c5227d762c840987d298f30823cca4eedaa
AlexeyVarenko <alexey_varenko@mail.ru>
2022-09-23 18:47:04 +0300

    Update Solutions_8kyu.js

--- Solutions_8kyu.js
+++ Solutions_8kyu.js
@@ -246,3 +246,12 @@ function checkForFactor (base, factor) {
       return false
     }
     }
+
+    /*Remove String Spaces
+
+    Simple, remove the spaces from the string, then return the resultant string.*/
+
+    function noSpace(x){
+        str = x.replace(/\s+/g, '');
+         return(str)
+       }
