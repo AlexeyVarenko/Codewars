1581cc925ea0a58e787047a005de1f1f3569afe0
AlexeyVarenko <alexey_varenko@mail.ru>
2022-10-14 15:06:42 +0300

    Update Solutions_7kyu.js

--- Solutions_7kyu.js
+++ Solutions_7kyu.js
@@ -1898,3 +1898,27 @@ var filterString = function(value) {
   let result=Number(str);
   return(result)
 }
+
+
+/*Count the divisors of a number
+Count the number of divisors of a positive integer n.
+
+Random tests go up to n = 500000.
+
+Examples (input --> output)
+4 --> 3 (1, 2, 4)
+5 --> 2 (1, 5)
+12 --> 6 (1, 2, 3, 4, 6, 12)
+30 --> 8 (1, 2, 3, 5, 6, 10, 15, 30)
+Note you should only return a number, the count of divisors. The numbers between parentheses are shown only for you to see which numbers are counted in each case.*/
+
+
+function getDivisorsCnt(n){
+  let result=[]
+  for (let i=1; i<=n; i++){
+    if (n%i==0){
+      result.push(i)
+    }
+  }
+return(result.length)
+}
