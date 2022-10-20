43ad3a522d44a5bd9c9e720d5b4a94af7e2d7622
AlexeyVarenko <alexey_varenko@mail.ru>
2022-10-20 11:39:35 +0300

    Update Solutions_7kyu.js

--- Solutions_7kyu.js
+++ Solutions_7kyu.js
@@ -2059,3 +2059,20 @@ function decode(s) {
   return s.length % 2 === 0? result.reverse().join('') : result.join('')
 
 }
+
+/*Find the Square Root
+Create a Function that takes one parameter and returns its square root rounded to 5 decimal places. You are not allowed to use the Math.sqrt() or the Math.pow() methods.
+
+Trailing zeros should be left out. For example
+
+squareRoot(39) // => 6.245;
+rather than
+
+squareRoot(39) // => 6.24500;
+*/
+
+
+function squareRoot(x) {
+  let result=Number((x**0.5).toFixed(5));
+  return(result)
+}
