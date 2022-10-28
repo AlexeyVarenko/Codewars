1065f4e67ed78525d26de5296a40d21f189005b3
AlexeyVarenko <alexey_varenko@mail.ru>
2022-10-28 19:49:24 +0300

    Update Solutions_7kyu.js

--- Solutions_7kyu.js
+++ Solutions_7kyu.js
@@ -2237,3 +2237,24 @@ const xMarksTheSpot = (input) => {
 	}
 	return (count !== 1 ? [] : [x, y]);
 };
+
+/*Squares sequence
+
+Complete the function that returns an array of length n, starting with the given number x and the squares of the previous number. If n is negative or zero, return an empty array/list.
+
+Examples
+2, 5  -->  [2, 4, 16, 256, 65536]
+3, 3  -->  [3, 9, 81]*/
+
+
+function squares(x, n) {
+  let result=[];
+    if (n>0){
+      for(i=0; i<n; i++){
+     result.push(x);
+      x=x**2;
+    }
+   return(result)
+    }else {
+    return([])
+  }}
