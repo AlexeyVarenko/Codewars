b0a81ab38aae5e3768173b61ee4a1afab23471d0
AlexeyVarenko <alexey_varenko@mail.ru>
2022-11-07 14:41:34 +0300

    Update Solutions_7kyu.js

--- Solutions_7kyu.js
+++ Solutions_7kyu.js
@@ -2362,3 +2362,32 @@ vowelOne( "aeiou, abc" ) // "1111100100"*/
 function vowelOne(s) {
   return s.replace(/[^aeiou]/gi, '0').replace(/[^\d]/g, '1');
 }
+
+/*Which are in?
+
+Given two arrays of strings a1 and a2 return a sorted array r in lexicographical order of the strings of a1 which are substrings of strings of a2.
+
+Example 1:
+a1 = ["arp", "live", "strong"]
+
+a2 = ["lively", "alive", "harp", "sharp", "armstrong"]
+
+returns ["arp", "live", "strong"]
+
+Example 2:
+a1 = ["tarp", "mice", "bull"]
+
+a2 = ["lively", "alive", "harp", "sharp", "armstrong"]
+
+returns []
+
+Notes:
+Arrays are written in "general" notation. See "Your Test Cases" for examples in your language.
+In Shell bash a1 and a2 are strings. The return is a string where words are separated by commas.
+Beware: In some languages r must be without duplicates.*/
+
+function inArray(array1,array2){
+  array2 = array2.join(' ');
+  let result = array1.filter(elem => array2.includes(elem)).sort();
+  return(result);  
+ }
