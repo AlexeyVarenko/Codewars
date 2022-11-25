a0d47c3000d4e3fb858c22ce480c74f4bd0cb3da
AlexeyVarenko <alexey_varenko@mail.ru>
2022-11-25 10:30:39 +0300

    Update Solutions_7kyu.js

--- Solutions_7kyu.js
+++ Solutions_7kyu.js
@@ -2506,3 +2506,82 @@ Jaden-Cased:     "How Can Mirrors Be Real If Our Eyes Aren't Real"*/
 String.prototype.toJadenCase = function() {
   return this.replace(/(^|\s)[a-z]/g, x => x.toUpperCase())
 }
+
+
+/*Thinking & Testing: A and B?
+
+No Story
+
+No Description
+
+Only by Thinking and Testing
+
+Look at result of testcase, guess the code!
+
+Series:
+01: A and B?
+02: Incomplete string
+03: True or False
+04: Something capitalized
+05: Uniq or not Uniq
+06: Spatiotemporal index
+07: Math of Primary School
+08: Math of Middle school
+09: From nothingness To nothingness
+10: Not perfect? Throw away!
+11: Welcome to take the bus
+12: A happy day will come
+13: Sum of 15(Hetu Luosliu)
+14: Nebula or Vortex
+15: Sport Star
+16: Falsetto Rap Concert
+17: Wind whispers
+18: Mobile phone simulator
+19: Join but not join
+20: I hate big and small
+21: I want to become diabetic ;-)
+22: How many blocks?
+23: Operator hidden in a string
+24: Substring Magic
+25: Report about something
+26: Retention and discard I
+27: Retention and discard II
+28: How many "word"?
+29: Hail and Waterfall
+30: Love Forever
+31: Digital swimming pool
+32: Archery contest
+33: The repair of parchment
+34: Who are you?
+35: Safe position
+Special recommendation
+Another series, innovative and interesting, medium difficulty. People who like to challenge, can try these kata:
+
+Play Tetris : Shape anastomosis
+Play FlappyBird : Advance Bravely*/
+
+
+function testit(a,b){
+  //return a+b ?
+  //return a*b ?
+  return a|b;
+}
+
+
+/*Leap Years
+
+
+In this kata you should simply determine, whether a given year is a leap year or not. In case you don't know the rules, here they are:
+
+years divisible by 4 are leap years
+but years divisible by 100 are not leap years
+but years divisible by 400 are leap years
+Additional Notes:
+
+Only valid years (positive integers) will be tested, so you don't have to validate them
+Examples can be found in the test fixture.*/
+
+
+function isLeapYear(year) {
+  return (year % 100 !== 0 && year % 4 === 0) || year % 400 === 0;
+ }
