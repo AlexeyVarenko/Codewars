91f641ec86214df94c0fc2107835a1bc3bf0d0fe
AlexeyVarenko <alexey_varenko@mail.ru>
2022-11-30 19:51:06 +0300

    Update Solutions_7kyu.js

--- Solutions_7kyu.js
+++ Solutions_7kyu.js
@@ -2639,3 +2639,48 @@ function londonCityHacker(journey) {
   } 
    return `£${sum.toFixed(2)}`;
  }
+
+
+ /*Find the smallest power higher than a given a value
+
+ We have the number 12385. We want to know the value of the closest cube but higher than 12385. The answer will be 13824.
+
+Now, another case. We have the number 1245678. We want to know the 5th power, closest and higher than that number. The value will be 1419857.
+
+We need a function find_next_power ( findNextPower in JavaScript, CoffeeScript and Haskell), that receives two arguments, a value val, and the exponent of the power, pow_, and outputs the value that we want to find.
+
+Let'see some cases:
+
+findNextPower(12385, 3) == 13824
+
+findNextPower(1245678, 5) == 1419857
+The value, val will be always a positive integer.
+
+The power, pow_, always higher than 1.
+
+Happy coding!!*/
+
+function findNextPower(val, pow_) {
+  for(let i=0; ;i++){
+   let result=Math.pow(i, pow_)
+    if(result>val){
+      return (result)
+    }
+  }
+  }
+
+  /*Square Every Digit
+
+  Welcome. In this kata, you are asked to square every digit of a number and concatenate them.
+
+For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1.
+
+Note: The function accepts an integer and returns an integer*/
+
+function squareDigits(num){
+  let arr= (""+num).split("").map(Number)
+  arr = arr.map(el => el **2);
+  let result = +arr.join('');
+  return(result)
+  
+}
