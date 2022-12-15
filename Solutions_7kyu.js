bd6ee96dfd312996ff627a02a5866c0e98c7d40e
AlexeyVarenko <alexey_varenko@mail.ru>
2022-12-15 20:30:37 +0300

    Update Solutions_7kyu.js

--- Solutions_7kyu.js
+++ Solutions_7kyu.js
@@ -2755,7 +2755,7 @@ Examples
 36     => 36
 1244   => 12
 952406 => 9
-}*/
+*/
 
 
 const prevMultOfThree = n => {
@@ -2764,4 +2764,20 @@ const prevMultOfThree = n => {
     n = Math.floor(n / 10);
   }
   return n;
+}
+
+/*Perpendicular lines
+
+
+You are given an input (n) which represents the amount of lines you are given, your job is to figure out what is the maximum amount of perpendicular bisectors you can make using these lines.
+
+Note: A perpendicular bisector is one that forms a 90 degree angle
+
+n will always be greater than or equal to 0
+Examples with 3 and 4 lines*/
+
+
+
+function maxBisectors(n) {
+  return Math.floor(n*n/4);
 }
