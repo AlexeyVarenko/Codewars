d832ace6923f816d47ac588e7ee66c32fe027b68
AlexeyVarenko <alexey_varenko@mail.ru>
2022-12-24 11:16:45 +0300

    Update Solutions_7kyu.js

--- Solutions_7kyu.js
+++ Solutions_7kyu.js
@@ -2804,3 +2804,30 @@ function findShort(s){
    }
    return shortestWord.length;
   }
+
+
+  /*
+Heron's formula
+
+Write function heron which calculates the area of a triangle with sides a, b, and c (x, y, z in COBOL).
+
+Heron's formula:
+
+s∗(s−a)∗(s−b)∗(s−c)\sqrt{s * (s - a) * (s - b) * (s - c)} 
+s∗(s−a)∗(s−b)∗(s−c)
+​
+ 
+where
+
+s=a+b+c2s = \frac{a + b + c} 2s= 
+2
+a+b+c
+​
+ 
+Output should have 2 digits precision.*/
+
+function heron(a, b, c) {
+  let s=(a+b+c)/2
+  let result = Math.sqrt(s*(s-a)*(s-b)*(s-c))
+  return Number(result.toFixed(2))
+}
