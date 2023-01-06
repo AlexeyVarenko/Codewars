f74f58874b1e08c2b3d61a6278b5ba40e121e884
AlexeyVarenko <alexey_varenko@mail.ru>
2023-01-06 19:58:53 +0300

    Update Solutions_7kyu.js

--- Solutions_7kyu.js
+++ Solutions_7kyu.js
@@ -2897,3 +2897,14 @@ function adjust(coin,price) {
     }
   }
  }
+
+ /*Count consonants
+
+ Write a function consonantCount, consonant_count or ConsonantCount that takes a string of English-language text and returns the number of consonants in the string.
+
+Consonants are all letters used to write English excluding the vowels a, e, i, o, u.*/
+
+function consonantCount(str) {
+  let result=str.replace(/[bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ]/gi, '')
+  return str.length-result.length
+ }
