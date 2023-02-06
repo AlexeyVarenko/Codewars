92b193c28762e9fe76a24c1668b808d3fd1c9ab8
AlexeyVarenko <alexey_varenko@mail.ru>
2023-02-06 20:21:35 +0300

    Update Solutions_7kyu.js

--- Solutions_7kyu.js
+++ Solutions_7kyu.js
@@ -3126,3 +3126,21 @@ arr.numberOfOccurrences(3) === 1;*/
 Array.prototype.numberOfOccurrences = function (n) {
   return this.filter(e => e === n).length
 }
+
+/*Convert Hash To An Array
+
+Convert a hash into an array. Nothing more, Nothing less.
+
+{name: 'Jeremy', age: 24, role: 'Software Engineer'}
+should be converted into
+
+[["age", 24], ["name", "Jeremy"], ["role", "Software Engineer"]]
+Note: The output array should be sorted alphabetically by key name.
+
+Good Luck!*/
+
+
+function convertHashToArray(hash){
+  let massiv = Object.entries(hash).sort();
+  return massiv
+}
