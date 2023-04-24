355e172453e3a034c311e6461c32da53cbe91b9a
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-04-24 10:01:42 +0300

    Update Solutions_7kyu.js

--- Solutions_7kyu.js
+++ Solutions_7kyu.js
@@ -3511,3 +3511,23 @@ function flattenAndSort(array) {
   let result=ABC.sort((a, b) => a - b);
   return(result);
 }
+
+
+/*
+Running out of space
+
+Kevin is noticing his space run out! Write a function that removes the spaces from the values and returns an array showing the space decreasing.
+For example, running this function on the array ['i', 'have','no','space'] would produce ['i','ihave','ihaveno','ihavenospace']
+
+*/
+
+
+function spacey(array){
+  let str = '';
+  let arr = [];
+  for(let i = 0; i < array.length; i++){
+    str = str + array[i];
+    arr.push(str);
+  }
+return arr;
+}
