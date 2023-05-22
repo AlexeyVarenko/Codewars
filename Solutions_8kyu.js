62ea00cc63de330ecd6a06dde459033093a1770b
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-05-22 10:27:29 +0300

    Update Solutions_8kyu.js

--- Solutions_8kyu.js
+++ Solutions_8kyu.js
@@ -568,3 +568,17 @@ function countBy(x, n) {
     }
     return z;
   }
+
+
+
+  /*Name Shuffler 
+  Write a function that returns a string in which firstname is swapped with last name.
+
+Example(Input --> Output)
+
+"john McClane" --> "McClane john"*/
+
+
+function nameShuffler(str){
+ return str.split(" ").reverse().join(" ")
+}
