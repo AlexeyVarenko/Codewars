29c67f96792877793a97e44a8cc72c9467d8bd21
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-05-22 10:32:56 +0300

    Update Solutions_8kyu.js

--- Solutions_8kyu.js
+++ Solutions_8kyu.js
@@ -582,3 +582,21 @@ Example(Input --> Output)
 function nameShuffler(str){
  return str.split(" ").reverse().join(" ")
 }
+
+
+/*Removing Elements
+
+
+Take an array and remove every second element from the array. Always keep the first element and start removing with the next element.
+
+Example:
+["Keep", "Remove", "Keep", "Remove", "Keep", ...] --> ["Keep", "Keep", "Keep", ...]
+
+None of the arrays will be empty, so you don't have to worry about that!*/
+
+
+function removeEveryOther(arr){
+  return arr.filter(function(elem, index) {
+    return index % 2 === 0;
+  });
+}
