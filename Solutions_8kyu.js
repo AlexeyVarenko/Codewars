8aea91b951eca2e2225fe64d2980e8eadba7f257
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-07-04 09:44:47 +0300

    Update Solutions_8kyu.js

--- Solutions_8kyu.js
+++ Solutions_8kyu.js
@@ -748,3 +748,23 @@ function sumMul(n, m) {
  }
   return res;
 }
+
+
+/*Triple Trouble
+
+Triple Trouble
+Create a function that will return a string that combines all of the letters of the three inputed strings in groups. Taking the first letter of all of the inputs and grouping them next to each other. Do this for every letter, see example below!
+
+E.g. Input: "aa", "bb" , "cc" => Output: "abcabc"
+
+Note: You can expect all of the inputs to be the same length.
+
+*/
+
+function tripleTrouble(one, two, three){
+	let answer = "";
+	for (let i = 0; i < one.length; i++){
+		answer += one[i] + two[i] + three[i];
+	}
+	return answer;
+}
