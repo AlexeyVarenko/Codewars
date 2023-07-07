c8eb270519e63a1799316273a2245d03482ba586
Alexey <90701779+AlexeyVarenko@users.noreply.github.com>
2023-07-07 10:49:57 +0300

    Update Solutions_8kyu.js

--- Solutions_8kyu.js
+++ Solutions_8kyu.js
@@ -804,3 +804,20 @@ function position(letter){
   const alphabet = 'abcdefghijklmnopqrstuvwxyz';
   return 'Position of alphabet: ' + (alphabet.indexOf(letter) + 1);
 }
+
+
+/*get character from ASCII Value
+
+Write a function get_char() / getChar() which takes a number and returns the corresponding ASCII char for that value.
+
+Example:
+
+get_char(65)
+should return:
+
+'A'*/
+
+
+function getChar(c){
+  return String.fromCharCode(c);
+}
